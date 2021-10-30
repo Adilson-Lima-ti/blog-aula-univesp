fdbc45aa6f6ec936906e2a15d6734e24f522930b
Adilson-Lima-ti <adilson.monte.alto@gmail.com>
2021-10-30 13:26:31 -0300

    commit

--- app.py
+++ app.py
@@ -1,10 +1,29 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for, flash, redirect
+import os, datetime
+import sqlite3
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.exceptions import abort
+project_dir = os.path.dirname(os.path.abspath(__file__))
+database_file = "sqlite:///{}".format(os.path.join(project_dir, "database.db"))
 
 app = Flask('__name__')
+app.config['SECRET_KEY'] = 'your secret key'
+app.config["SQLALCHEMY_DATABASE_URI"] = database_file
+db = SQLAlchemy(app)
+
+
+class Posts(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    created = db.Column(db.DateTime, default=datetime.datetime.utcnow)
+    title = db.Column(db.String(80), nullable=False)
+    content = db.Column(db.String(200), nullable=False)
+
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    posts = Posts.query.all()
+    return render_template('index.html', posts=posts)
+
 
 
     
